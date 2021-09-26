57417be838e520adc84117ae698e1543be43e64d
Serdar-Kaya <68328599+Serdar-Kaya@users.noreply.github.com>
2021-09-26 15:17:12 +0300

    Some changes

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 require([
+  "esri/WebScene",
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/GeoJSONLayer",
-], function (Map, SceneView, Map_1, SceneView_1) {
+], function (WebScene, Map, SceneView) {
+  const webscene = new WebScene({
+    basemap: null,
+    ground: {
+      surfaceColor: [226, 240, 255],
+    },
+  });
+
   const map = new Map({
     basemap: "topo",
   });
   const view = new SceneView({
     map: map,
     container: "viewDiv",
-  });
-
-  ("use strict");
-  Object.defineProperty(exports, "__esModule", { value: true });
-  var Map_1 = require("esri/Map");
-  var SceneView_1 = require("esri/views/SceneView");
-  var map = new Map_1.default({
-    basemap: "topo",
-  });
-  var view = new SceneView_1.default({
-    map: map,
-    container: "viewDiv",
+    environment: {
+      background: {
+        type: "color",
+        color: [0, 255, 225, 0.5],
+      },
+      starsEnabled: false,
+      atmosphereEnabled: false,
+    },
   });
 
   view.ui.empty("top-left");
